156363df735fa46d084a043b33497517158433d3
therivernix <therivernix@tommygeusens.com>
2026-06-24 03:33:51 +0200

    adding code to shell script to auto enable extensions

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -12,3 +12,30 @@ rm -rf /usr/share/gnome-shell/extensions/launch-new-instance@gnome-shell-extensi
 rm -rf /usr/share/gnome-shell/extensions/places-menu@gnome-shell-extensions.gcampax.github.com
 rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com
 rm -rf /usr/share/gnome-shell/extensions/logomenu@aryan_k
+
+extensions=(
+  "AlphabeticalAppGrid@stuarthayhurst"
+  "appindicatorsupport@rgcjonas.gmail.com"
+  "blur-my-shell@aunetx"
+  "caffeine@patapon.info"
+  "clipboard-indicator@tudmotu.com"
+  "custom-command-list@storageb.github.com"
+  "dash-to-dock@micxgx.gmail.com"
+  "hide-minimized@danigm.net"
+  "hotedge@jonathan.jdoda.ca"
+  "just-perfection-desktop@just-perfection"
+  "light-style@gnome-shell-extensions.gcampax.github.com"
+  "nightthemeswitcher@romainvigier.fr"
+  "quick-settings-audio-panel@rayzeq.github.io"
+  "restartto@tiagoporsch.github.io"
+  "smile-extension@mijorus.it"
+  "tailscale-gnome-qs@tailscale-qs.github.io"
+  "tiling-assistant@leleat-on-github"
+)
+
+for ext in "${extensions[@]}"; do
+  echo "Enabling: $ext"
+  gnome-extensions enable "$ext"
+done
+
+echo "All extensions enabled."
